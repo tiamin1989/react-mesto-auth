c32f940cbbfd85d60543e97591d41b97f0edc5c7
Ruslan Ramazanov <tiamin1989@yandex.ru>
2020-10-12 18:50:46 +0500

    minor fixes

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ function Header({ loggedIn, userData, onClick }) {
       <img src={logo} alt="Логотип Mesto - Russia" className="header__logo" />
       <div className="header__auth-wrapper">
         <span className="header__auth-email">{loggedIn ? userData.email : ''}</span>
-        <a className="header__auth-action" href={loggedIn ? '/' : '/sign-in'} onClick={onClick}>{loggedIn ? 'Выйти' : 'Регистрация'}</a>
+        <a className="header__auth-action" href={loggedIn ? '/' : '/sign-up'} onClick={onClick}>{loggedIn ? 'Выйти' : 'Регистрация'}</a>
       </div>
     </header>
   )
