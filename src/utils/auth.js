5ef7b6688ac91deb13e456823ae6edeafe31d79c
Ruslan Ramazanov <tiamin1989@yandex.ru>
2020-10-19 10:50:33 +0500

    change server

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://api.tiamin1989.students.nomoreparties.co';
+export const BASE_URL = 'https://api.tiamin1989.students.nomoreparties.xyz';
 
 export const register = (email, password) => {
   return fetch(`${BASE_URL}/signup`, {
