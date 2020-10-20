818c704608a466aec76ddd2508f760ce8de43ec9
Ruslan Ramazanov <tiamin1989@yandex.ru>
2020-10-20 23:03:13 +0500

    change https to http

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://api.tiamin1989.students.nomoreparties.xyz';
+export const BASE_URL = 'http://api.tiamin.students.nomoreparties.xyz';
 
 export const register = (email, password) => {
   return fetch(`${BASE_URL}/signup`, {
