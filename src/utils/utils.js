0d154229bfbad28b4f4e05c5b53a2120356b7299
Ruslan Ramazanov <tiamin1989@yandex.ru>
2020-10-25 17:15:43 +0500

    change servers

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,8 +1,8 @@
 import Api from './api.js';
 
 export const connectApi = new Api({
-  /* baseUrl: 'https://api.tiamin.students.nomoreparties.space', */
-  baseUrl: 'http://localhost:3000',
+  baseUrl: 'https://api.tiamin.students.nomoreparties.space',
+  /* baseUrl: 'http://localhost:3000', */
   headers: {
     'Content-Type': 'application/json'
   }
